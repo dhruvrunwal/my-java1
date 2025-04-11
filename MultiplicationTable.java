c57fcb0c1e4806d930d752493744aed97673bb04
dhruvrunwal <24runwald@rbunagpur.in>
2025-04-12 02:17:20 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
-        int num = 7;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        printMultiplicationTable(num);
+    }
+
+    public static void printMultiplicationTable(int num) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + " x " + i + " = " + (num * i));
         }
