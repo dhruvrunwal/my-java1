6ee9f78b2e491bd7f922945161fa31f490f87bdf
dhruvrunwal <24runwald@rbunagpur.in>
2025-04-12 02:20:40 +0530

    Create MultilevelInheritance.java

---
+++ MultilevelInheritance.java
@@ -0,0 +1,25 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Mammal extends Animal {
+    void breathe() {
+        System.out.println("Mammal breathes air");
+    }
+}
+
+class Dog extends Mammal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();   // Calls Dog's sound method
+        d.breathe(); // Inherited from Mammal
+    }
+}
